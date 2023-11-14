32393b45c290fe2d2fe94fd24b5128b7b58bd124
future4ing <mirae4120@gmail.com>
2023-11-14 19:09:15 +0900

    feat : 식당 방문 날짜 입력받는 기능 구현
    - 식당 방문 날짜를 입력받음
    - 예외 처리 : 입력값이 1 이상 31 이하의 숫자가 아닌 경우

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Console } from "@woowacourse/mission-utils";
+import InputView from './InputView.js';
 import { MESSAGE_GREETING } from './constants.js';
 
 class App {
   async run() {
     Console.print(MESSAGE_GREETING);
-
+    const input = new InputView;
+    input.readDate();
   }
 }
 
--- src/InputView.js
+++ src/InputView.js
@@ -1,7 +1,35 @@
-export default InputView = {
+/* eslint-disable import/no-cycle */
+import { Console } from '@woowacourse/mission-utils';
+import { INPUT_DATE, ERROR } from './constants.js';
+
+class InputView {
+    constructor() {
+        this.input = 0;
+    }
+    
     async readDate() {
-        const input = await Console.readLineAsync("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
-        // ...
+        while (true) {
+            try {
+                const input = await Console.readLineAsync(INPUT_DATE);
+    
+                const dateInput = parseInt(input, 10);
+                if (isNaN(dateInput) || dateInput < 1 || dateInput > 31) {
+                    throw new Error(ERROR.invalidDate);
+                }
+                this.input = dateInput;
+                
+                return this.input;
+    
+            } catch (error) {
+                // 예외 처리: 에러 메시지 출력 또는 다른 처리를 추가할 수 있습니다.
+                console.error(error.message);
+            }
+        }
+
+        
     }
     // ...
 }
+
+
+export default InputView;
