bd311ed6972c44167e7ee6d6de2f0a9a5e531c8c
future4ing <mirae4120@gmail.com>
2023-11-14 19:07:14 +0900

    feat : 인사말을 출력하는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import { Console } from "@woowacourse/mission-utils";
+import { MESSAGE_GREETING } from './constants.js';
+
 class App {
-  async run() {}
+  async run() {
+    Console.print(MESSAGE_GREETING);
+
+  }
 }
 
 export default App;
