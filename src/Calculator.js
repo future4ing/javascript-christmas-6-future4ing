aa2a898c56cd86d740d8faf7195a75c28266ba3b
future4ing <mirae4120@gmail.com>
2023-11-15 21:50:25 +0900

    refactor : 할인 전 총 주문 비용을 계산하는 기능 리팩토링
    - reduce 사용
    - 주문 항목별 계산을 별도 함수로 분리

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,22 +1,17 @@
 import menu from './Menu.js';
 
-function calculateOrderCost(menu, order) {
-    let totalCost = 0;
-    // 주문을 각 메뉴별로 분리 (예: "해산물파스타-2,레드와인-1,초코케이크-1")
-    const orderedItems = order.split(',');
-    // 주문 금액 계산 로직 구현
-    for (const item of orderedItems) {
-        // 각 항목을 이름과 수량으로 분리 (예: "해산물파스타-2" -> ["해산물파스타", "2"])
-        const [itemName, quantityStr] = item.split('-');
-        const quantity = parseInt(quantityStr, 10);
+function calculateItemCost(menu, item) {
+    const [itemName, quantityStr] = item.split('-');
+    const quantity = parseInt(quantityStr, 10);
+    const menuItem = menu.find(m => m.name === itemName.trim());
 
-        // 메뉴에서 해당 항목의 가격 찾기
-        const menuItem = menu.find(m => m.name === itemName.trim());
-        if (menuItem) {
-            totalCost += menuItem.price * quantity;
-        }
-    }
-    return totalCost;
+    return menuItem ? menuItem.price * quantity : 0;
+}
+
+function calculateOrderCost(menu, order) {
+    const orderedItems = order.split(',');
+
+    return orderedItems.reduce((total, item) => total + calculateItemCost(menu, item), 0);
 }
 
 function isWeekend(date) {
