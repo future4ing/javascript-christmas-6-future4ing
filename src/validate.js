4207b58508651e28cf6d2820c3b80ac864516d83
future4ing <mirae4120@gmail.com>
2023-11-15 23:05:30 +0900

    feat : 입력값 유효성 검사 기능 구현

--- src/validate.js
+++ src/validate.js
@@ -1,9 +1,72 @@
-import { ERROR } from './constants.js';
+import { ERROR, MIN_DATE, MAX_DATE } from './constants.js';
+
+const menuList = {
+    "양송이수프": true,
+    "타파스": true,
+    "시저샐러드": true,
+    "티본스테이크": true,
+    "바비큐립": true,
+    "해산물파스타": true,
+    "크리스마스파스타": true,
+    "초코케이크": true,
+    "아이스크림": true,
+    "제로콜라": true,
+    "레드와인": true,
+    "샴페인": true
+  };
 
 function validateVisitDate(visitDate) {
-    if (isNaN(visitDate) || visitDate < 1 || visitDate > 31) {
+    if (isNaN(visitDate) || visitDate < MIN_DATE || visitDate > MAX_DATE) {
         throw new Error(ERROR.invalidDate);
     }
 }
 
-export { validateVisitDate };
+function isValidOrderMenu(orderMenu) {
+  const menuRegex = /^[가-힣\s]+-\d+$/;
+  const validMenus = new Set();
+
+  const menuEntries = orderMenu.split(',');
+  for (const menuEntry of menuEntries) {
+    validateMenuEntry(menuEntry, validMenus);
+  }
+
+  return true;
+}
+
+function validateMenuEntry(menuEntry, validMenus) {
+  const trimmedMenuEntry = menuEntry.trim();
+  const [menuName, quantity] = trimmedMenuEntry.split('-');
+
+  if (!isValidMenuFormat(trimmedMenuEntry)) {
+    throw new Error(ERROR.invalidMenuFormat);
+  }
+
+  if (!isValidMenuName(menuName)) {
+    throw new Error(ERROR.orderNotOnMenu);
+  }
+
+  if (!isValidMenuCount(quantity)) {
+    throw new Error(ERROR.invalidMenuCount);
+  }
+
+  if (validMenus.has(menuName)) {
+    throw new Error(ERROR.duplicateMenu);
+  }
+
+  validMenus.add(menuName);
+}
+
+function isValidMenuFormat(menuEntry) {
+  const menuRegex = /^[가-힣\s]+-\d+$/;
+  return menuRegex.test(menuEntry);
+}
+
+function isValidMenuName(menuName) {
+    return menuList[menuName] === true;
+}
+
+function isValidMenuCount(quantity) {
+  return !isNaN(quantity) && quantity >= 1;
+}
+
+export { validateVisitDate, isValidOrderMenu };
