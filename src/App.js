c8c6fd1af76737f21aba63160990b5e14a1d3846
future4ing <mirae4120@gmail.com>
2023-11-15 22:12:19 +0900

    style : App의 주석 삭제

--- src/App.js
+++ src/App.js
@@ -5,15 +5,14 @@ class App {
     async run() {
         const outputView = new OutputView();
         const inputView = new InputView();
-        // 인사말 출력
+
         outputView.printGreeting();
-        // 방문 날짜, 메뉴 입력 받음
+ 
         const date = await inputView.readVisitDate();
         const order = await inputView.readOrderMenu();
-        // 미리보기 안내 문구 출력
+   
         outputView.printPreview(date);
         
-        // 이벤트 혜택 미리보기 출력
         outputView.printPreviewDetail(order, date);
     }
 }
