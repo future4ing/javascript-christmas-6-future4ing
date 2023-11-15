05b33f4d1102f9c861cbc64dd7b7ec8d6771f958
future4ing <mirae4120@gmail.com>
2023-11-15 15:53:43 +0900

    feat : 증정 메뉴 관련 상수 추가, 수정
    - add GIFT
    - fix GIVE_MENU > GIFT_MENU

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,3 @@
-
 const INPUT_DATE =
   '12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)\n';
 const INPUT_ORDER = '주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)\n';
@@ -13,22 +12,23 @@ const ERROR = {
   invalidTotalCount: `${ERROR_PREFIX} 메뉴는 한 번에 최대 20개까지만 주문할 수 있습니다. 다시 입력해 주세요.`,
   orderOnlyDrink: `${ERROR_PREFIX} 음료만 주문 시, 주문할 수 없습니다. 다시 입력해 주세요.`,
 };
-
+ 
 const MESSAGE_GREETING = '안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.';
-const MESSAGE_PREVIEW = `12월 00일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n`;
+const MESSAGE_PREVIEW = '12월 ${date}일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n';
 
 const TITLE_BRACKET_BEFORE = '<';
 const TITLE_BRACKET_AFTER = '>';
 const TITLE = {
     ORDER_MENU: `${TITLE_BRACKET_BEFORE}주문 메뉴${TITLE_BRACKET_AFTER}`,
     TOTAL_AMOUNT: `${TITLE_BRACKET_BEFORE}할인 전 총주문 금액${TITLE_BRACKET_AFTER}`,
-    GIVE_MENU: `${TITLE_BRACKET_BEFORE}증정 메뉴${TITLE_BRACKET_AFTER}`,
+    GIFT_MENU: `${TITLE_BRACKET_BEFORE}증정 메뉴${TITLE_BRACKET_AFTER}`,
     BENEFIT: `${TITLE_BRACKET_BEFORE}혜택 내역${TITLE_BRACKET_AFTER}`,
     TOTAL_BENEFIT: `${TITLE_BRACKET_BEFORE}총혜택 금액${TITLE_BRACKET_AFTER}`,
     AMOUNT_AFTER_DISCOUNT: `${TITLE_BRACKET_BEFORE}할인 후 예상 결제 금액${TITLE_BRACKET_AFTER}`,
     EVENT_BADGE: `${TITLE_BRACKET_BEFORE}12월 이벤트 배지${TITLE_BRACKET_AFTER}`,
 };
 
+const GIFT = '샴페인 1개';
 const NO_BENEFIT = '없음';
 
 export {
@@ -38,5 +38,6 @@ export {
     MESSAGE_GREETING,
     MESSAGE_PREVIEW,
     TITLE,
+    GIFT,
     NO_BENEFIT,
 };
