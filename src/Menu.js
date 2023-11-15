f4215e0f01e84de5a39d638afda8d98142be1bb4
future4ing <mirae4120@gmail.com>
2023-11-15 20:23:04 +0900

    feat : 메뉴 목록을 객체 배열을 추가

---
+++ src/Menu.js
@@ -0,0 +1,16 @@
+const menu = [
+    { name: '양송이수프', price: 6000, section: 'appetizer' },
+    { name: '타파스', price: 5500, section: 'appetizer' },
+    { name: '시저샐러드', price: 8000, section: 'appetizer' },
+    { name: '티본스테이크', price: 55000, section: 'main' },
+    { name: '바비큐립', price: 54000, section: 'main' },
+    { name: '해산물파스타', price: 35000, section: 'main' },
+    { name: '크리스마스파스타', price: 25000, section: 'main' },
+    { name: '초코케이크', price: 15000, section: 'dessert' },
+    { name: '아이스크림', price: 5000, section: 'dessert' },
+    { name: '제로콜라', price: 3000, section: 'beverage' },
+    { name: '레드와인', price: 60000, section: 'beverage' },
+    { name: '샴페인', price: 25000, section: 'beverage' }
+];
+
+export default menu;
