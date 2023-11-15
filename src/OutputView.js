2394021515a091a4a7d78e83283149c660f95adc
future4ing <mirae4120@gmail.com>
2023-11-15 20:20:14 +0900

    feat : 혜택 내역을 가져오는 기능 구현

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,7 +1,7 @@
 import { Console } from '@woowacourse/mission-utils';
 import { MESSAGE_GREETING, MESSAGE_PREVIEW } from './constants.js';
 import PreviewDetail from './PreviewDetail.js';
-import { calculateOrderCost } from './Calculator.js';
+import { calculateOrderCost, calculateEventDiscounts } from './Calculator.js';
 import menu from './Menu.js';
 
 class OutputView {
@@ -14,7 +14,7 @@ class OutputView {
         Console.print(message);
     }
 
-    printPreviewDetail(order) {
+    printPreviewDetail(order, date) {
         const details = new PreviewDetail();
         // 주문 메뉴
         details.printMenu(order);
@@ -23,6 +23,16 @@ class OutputView {
         details.printTotalOrderPrice(totalOrderPrice);
         // <증정 메뉴>
         details.printGiftMenu(totalOrderPrice);
+        // <혜택 내역>
+        // 숫자 형태의 날짜를 문자열로 변환, 필요한 경우 앞에 '0'을 추가
+        function convertNumberToDate(date) {
+            const formattedDay = date.toString().padStart(2, '0');
+            return new Date(`2023-12-${formattedDay}`);
+        }
+        const dateObj = convertNumberToDate(date);
+
+        const discounts = calculateEventDiscounts(dateObj, order, totalOrderPrice);
+        details.printBenefits(discounts);
     }
 }
 
